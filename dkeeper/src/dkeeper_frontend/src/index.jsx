106f6cb9ec24251ae8a4f588f31a03e4162cf8ee
Abdullah-1057 <abdullah.hkt@gmail.com>
2023-09-28 11:17:55 +0500

    first commit

---
+++ dkeeper/src/dkeeper_frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+// import { dkeeper_backend } from "../../declarations/dkeeper_backend/index";
+ReactDOM.render(<App />, document.getElementById("root"));
